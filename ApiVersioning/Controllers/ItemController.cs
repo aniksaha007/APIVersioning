88feff345945930469b6c1620b9835f751367b45
Anik Saha <82827610+aniksaha007@users.noreply.github.com>
2022-01-09 10:05:45 +0600

    Initial Commit

---
+++ ApiVersioning/Controllers/ItemController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiVersioning.Controllers
+{
+    [ApiVersion("1.0")]
+    [ApiVersion("2.0")]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    //[Route("api/[controller]")]
+    [ApiController]
+    public class ItemController : ControllerBase
+    {
+        [HttpGet("{id}")]
+        public IActionResult GetItemv1([FromRoute] int id)
+        {
+            var item = new Itemv1
+            {
+                Id = id,
+                Name = "ABC"
+            };
+            return Ok(item);
+        }
+
+        [HttpGet("{id}")]
+        [MapToApiVersion("2.0")]
+        public IActionResult GetItemv2([FromRoute] int id)
+        {
+            var item = new Itemv2
+            {
+                Id = id,
+                ProductName = "XYZ"
+            };
+            return Ok(item);
+
+        }
+    }
+}
---
+++ ApiVersioning/Item.cs
@@ -0,0 +1,18 @@
+namespace ApiVersioning
+{
+    public class Itemv1
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+    }
+
+    public class Itemv2
+    {
+        public int Id { get; set; }
+
+        public string ProductName { get; set; }
+
+    }
+}
